78ebeeb1a912959a0aacb8961fb1ee6b9586b1b7
Frank P Moley III <frankmoley@gmail.com>
2019-05-16 12:10:57 -0500

    Initial project checkin

---
+++ src/main/java/com/frankmoley/accelerate/starwarscharacterservice/StarwarsCharacterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.frankmoley.accelerate.starwarscharacterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsCharacterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarwarsCharacterServiceApplication.class, args);
+    }
+
+}
